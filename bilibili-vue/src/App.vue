7662a8f78678b5c2ed91639f1f183897d18305ad
tmflsby <823246840@qq.com>
2020-10-13 14:10:25 +0800

    bilibili-vue: 引入normalize.css/新增PostMaterial,TopContainer组件

--- bilibili-vue/src/App.vue
+++ bilibili-vue/src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <TopContainer></TopContainer>
   </div>
 </template>
 
-<style lang="stylus">
+<script>
+import TopContainer from "./components/commom/TopContainer";
+export default {
+  name: "App",
+  components: { TopContainer }
+}
+</script>
+
+<style lang="stylus" scoped>
 #app
-  font-family Avenir, Helvetica, Arial, sans-serif
+  font-family "Microsoft YaHei",Arial,Helvetica,sans-serif
   -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  font-size 12px
+  margin 0
+  padding 0
+  background #fff
+  color #222
+  min-width 990px
+  tap-highlight-color transparent
+  -webkit-tap-highlight-color transparent
 </style>
